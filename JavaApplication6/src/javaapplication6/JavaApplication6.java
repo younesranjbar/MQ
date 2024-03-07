240b3ff2b14dbcbf3788e9e6f12d9083436eba6d
younesranjbar <younes.ranjbar769@gmail.com>
2024-03-07 19:37:01 +0330

    Sum the number and display its output

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -17,6 +17,10 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
     }
     
 }
